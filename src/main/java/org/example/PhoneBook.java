5818b4dd481b1d8fbdc10f5f202d6b703c5cbe3e
Danis Abdulgazizov <businessskyjoyboy@gmail.com>
2023-10-05 19:42:54 +0500

    refactoring add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -4,16 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PhoneBook {
-    static Map<String, String> contacts = new HashMap<>();
+    private Map<String, String> nameToNumber = new HashMap<>();
+    private Map<String, String> numberToName = new HashMap<>();
 
 
-    public static void main(String[] args) {
-
-    }
 
     public int add(String name, String number) {
-        contacts.put(name, number);
-        return contacts.size();
+        nameToNumber.put(name, number);
+        return nameToNumber.size();
     }
 
     public String findByNumber(String number) {
